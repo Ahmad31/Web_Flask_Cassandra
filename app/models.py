85a27c61d7e7518b7b17dfa59d8b0d43195c7531
Ahmad31 <aku.anwar.aan@gmail.com>
2017-06-29 20:20:20 +0700

    tambahan update semua jurusan

--- app/models.py
+++ app/models.py
@@ -3,28 +3,33 @@ from app import app
 from werkzeug import generate_password_hash, check_password_hash
 
 
-class Document(db.Model):
-    __table_name__ = 'document'
+class Dokumen(db.Model):
+    __table_name__ = 'dokumen'
 
     nim = db.columns.Integer(primary_key=True)
     prodi = db.columns.Text(primary_key=True)
-    tahun = db.columns.Date(primary_key=True, clustering_order="DESC")
-    judul = db.columns.Text(primary_key=True)
+    tahun = db.columns.Integer()
+    judul = db.columns.Text()
     nama_mhs = db.columns.Text()
-    angkatan = db.columns.Date()
+    angkatan = db.columns.Integer()
     intisari = db.columns.Text()
     kata_kunci = db.columns.Text()
     pembimbing = db.columns.Text()
-    file_doc = db.columns.Text()
+    file1 = db.columns.Text()
+    file2 = db.columns.Text()
+    file3 = db.columns.Text()
+    file4 = db.columns.Text()
+    file5 = db.columns.Text()
     password = db.columns.Text()
 
     def __repr__(self):
-        return '<Document %r>' % (self.nim, self.nama_mhs, self.angkatan, self.tahun, self.prodi, self.judul, self.kata_kunci, self.intisari, self.pembimbing, self.password, self.file_doc)
+        return '<Dokumen %r>' % (self.nim, self.nama_mhs, self.angkatan, self.tahun, self.prodi, self.judul, self.kata_kunci, self.intisari, self.pembimbing, self.password, self.file1, self.file2, self.file3, self.file4, self.file5)
 
 class User(db.Model):
     __table_name__ = "user"
     username = db.columns.Text(primary_key=True)
     password = db.columns.Text(primary_key=True)
+    alamat = db.columns.Text()
 
     def is_active(self):
         return True
@@ -36,7 +41,7 @@ class User(db.Model):
         return unicode(self.username)
 
     def __repr__(self):
-        return '<User %r>' % (self.username, self.password)
+        return '<User %r>' % (self.username, self.password, self.alamat)
 
 
 
--- app/views.py
+++ app/views.py
@@ -1,8 +1,9 @@
 from app import app, login_manager 
 from flask import Flask, abort, render_template, session, redirect, url_for, escape, request, flash, g, Response
-from .models import db, User_admin, Document
+from .models import db, Dokumen
 from .forms import LoginAdminForm
 from cassandra.cluster import Cluster
+from datetime import datetime
 
 cluster = Cluster(['172.17.0.2'])
 sesi = cluster.connect()
@@ -11,7 +12,7 @@ sesi.execute("USE project")
 
 @app.route('/detil/<judul>-<nim>')
 def detil_doc(nim, judul):
-	detil = Document.objects(nim = nim)
+	detil = Dokumen.objects(nim = nim).allow_filtering()
 	return render_template("document.html", detil=detil)
 
 @app.route('/')
@@ -21,23 +22,16 @@ def index():
 
 @app.route('/form_search')
 def form_search():
-	data = Document.objects().all()
-	return render_template("search_document.html", data=data)
-
-
-@app.route('/all_data')
-def all_data():
-	data = Document.objects().all()
-	return render_template("tampil_all_data.html", data=data)
+	data = Dokumen.objects().all()
+	return render_template("form_search.html", data=data)
 
 @app.route('/search', methods=['GET','POST'])
 def search():
 	""" cari, query cari """
-	cari_judul = sesi.execute(" SELECT * FROM document WHERE judul LIKE  '%{}%'".format(request.form['cari']))
+	cari_judul = sesi.execute(" SELECT * FROM dokumen WHERE judul LIKE  '%{}%'".format(request.form['cari']))
 	info = request.form['cari']
 	return render_template("hasil_search.html", cari_judul=cari_judul, info=info)
-
-
+	
 @app.route('/admin')
 def admin():
 	if not session.get('logged_in'):
@@ -50,7 +44,6 @@ def admin():
 @app.route('/login')
 def login():
 	return render_template("login.html")
-
 
 @app.route('/login_admin', methods=['POST'])
 def login_admin():
@@ -67,9 +60,6 @@ def logout():
     session['logged_in'] = False
     return admin()
 
-@login_manager.user_loader
-def load_login(username):
-	return User.get(text(username))
 
 @app.route('/add', methods = ['GET', 'POST'])
 def add():
@@ -78,7 +68,7 @@ def add():
 		if not request.form['nim'] or not request.form['judul']:
 			error = "Masukkan Data dengan komplit dan sesuai!"
 		else:
-			doc = Document( nim=request.form['nim'],
+			doc = Dokumen( nim=request.form['nim'],
 							nama_mhs=request.form['nama'],
 							angkatan=request.form['angkatan'],
 							tahun=request.form['Tlulus'],
@@ -99,7 +89,7 @@ def add():
 			flash('Sukses menambah data')
 			return redirect(url_for('index'))
 
-	return render_template('input_data.html')
+	return redirect(url_for('data_doc'))
 
 @app.route('/userAdd', methods = ['GET', 'POST'])
 def addUser():
@@ -116,3 +106,54 @@ def addUser():
 
 	return render_template('add_user.html')
 
+@app.route('/data_doc', methods = ['GET','POST'])
+def data_doc():
+	now = datetime.now()
+	now = now.strftime("%Y")
+	angkatan_date = reversed(range(int(now)-10, int(now)+1))
+	tahun_date = reversed(range(int(now)-10, int(now)+1))
+	data_ti = Dokumen.objects.filter(prodi='Teknik Informatika').allow_filtering()
+	data_si = Dokumen.objects.filter(prodi='Sistem Informasi').allow_filtering()
+	data_tk = Dokumen.objects.filter(prodi='Teknik Komputer').allow_filtering()
+	data_mi = Dokumen.objects.filter(prodi='Management Informatika').allow_filtering()
+	data_ka = Dokumen.objects.filter(prodi='Komputer Akutansi').allow_filtering()
+
+	return render_template("data_doc.html", data_ti=data_ti, data_si=data_si, data_tk=data_tk, data_mi=data_mi, data_ka=data_ka, date1=angkatan_date, date2=tahun_date)
+
+
+@app.route('/edit_doc/<nim>', methods = ['GET','POST'])
+def edit_doc(nim):
+	now = datetime.now()
+	now = now.strftime("%Y")
+	angkatan_date = reversed(range(int(now)-10, int(now)+1))
+	tahun_date = reversed(range(int(now)-10, int(now)+1))
+	view_doc = Dokumen.objects(nim = nim).allow_filtering()
+	return render_template("edit_doc.html", view_doc=view_doc, date1=angkatan_date, date2=tahun_date) 
+
+@app.route('/respon_edit', methods=['GET','POST'])
+def respon_edit():
+	if request.method == 'POST':
+		if not request.form['nama_m']:
+			error = "Masukkan Data dengan komplit dan sesuai!"
+		else:
+			data = Dokumen.objects(prodi='Teknik Informatika', nim=request.form['nim_mhs']).update(
+									nama_mhs=request.form['nama_m'],
+									angkatan=request.form['angkatan_mhs'],
+									tahun=request.form['tlulus_mhs'],
+									judul=request.form['judul_doc'],
+									kata_kunci=request.form['kunci_kata'],
+									intisari=request.form['intisari_doc'],
+									pembimbing=request.form['pembimbing_mhs']
+									)
+			error = "Maaf Isi Data Secara Lengkap"
+			return redirect(url_for('data_doc'))
+
+	return redirect(url_for('data_doc'))
+
+@app.route('/delete_doc/<nim>', methods=['GET','POST'])
+def delete_doc(nim):
+	return render_template("delete_doc.html", nim=nim) 
+
+@app.route('/respon_delet', methods=['GET','POST'])
+def respon_delet():
+	return render_template("data_doc.html", delet=delet) 
